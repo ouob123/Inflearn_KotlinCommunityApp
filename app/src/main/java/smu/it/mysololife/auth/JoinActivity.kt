10855408b37a3a0c7994f431b01620e806f1c348
ouob <kyjin320@sookmyung.ac.kr>
2023-05-12 02:40:36 +0900

    Community App

---
+++ app/src/main/java/smu/it/mysololife/auth/JoinActivity.kt
@@ -0,0 +1,14 @@
+package smu.it.mysololife.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import smu.it.mysololife.R
+
+class JoinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
---
+++ app/src/main/java/smu/it/mysololife/auth/LoginActivity.kt
@@ -0,0 +1,14 @@
+package smu.it.mysololife.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import smu.it.mysololife.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
