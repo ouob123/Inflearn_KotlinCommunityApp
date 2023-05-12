ed97a1eaf6e608363d359748b092d200516ab8ab
ouob <kyjin320@sookmyung.ac.kr>
2023-05-13 02:53:55 +0900

    Community App(Section 2)

--- app/src/main/java/smu/it/mysololife/auth/IntroActivity.kt
+++ app/src/main/java/smu/it/mysololife/auth/IntroActivity.kt
@@ -4,15 +4,24 @@ import android.content.Intent
 import android.graphics.Paint.Join
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import smu.it.mysololife.MainActivity
 import smu.it.mysololife.R
 import smu.it.mysololife.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+
     private lateinit var binding: ActivityIntroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = Firebase.auth
 
         super.onCreate(savedInstanceState)
         // setContentView(R.layout.activity_intro)
@@ -27,6 +36,24 @@ class IntroActivity : AppCompatActivity() {
         binding.joinBtn.setOnClickListener{
             val intent = Intent(this, JoinActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.noAccountBtn.setOnClickListener{
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+
+                        val intent =  Intent(this, MainActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        startActivity(intent)
+
+                    } else {
+
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                    }
+                }
         }
     }
 }
--- app/src/main/java/smu/it/mysololife/auth/JoinActivity.kt
+++ app/src/main/java/smu/it/mysololife/auth/JoinActivity.kt
@@ -1,14 +1,103 @@
 package smu.it.mysololife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import smu.it.mysololife.MainActivity
 import smu.it.mysololife.R
+import smu.it.mysololife.databinding.ActivityJoinBinding
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var binding : ActivityJoinBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_join)
+
+        binding.joinBtn.setOnClickListener{
+
+            var isGoToJoin = true
+
+            val email = binding.emailArea.text.toString()
+            val password1 = binding.passwordArea1.text.toString()
+            val password2 = binding.passwordArea2.text.toString()
+
+            // 저기 값이 비어있는지 확인
+            if(email.isEmpty()) {
+                Toast.makeText(this, "이메일을 입력해주세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            if(password1.isEmpty()){
+                Toast.makeText(this, "password1을 입력해주세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            if(password2.isEmpty()){
+                Toast.makeText(this, "password2을 입력해주세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            // 비밀번호 2개가 같은지 확인
+            if(!password1.equals(password2)) {
+                Toast.makeText(this, "비밀번호를 똑같이 입력해주세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            // 비밀번호가 6자 이상인지
+            if (password1.length < 6) {
+                Toast.makeText(this, "비밀번호를 6자리 이상으로 입력해주세요", Toast.LENGTH_LONG).show()
+                isGoToJoin = false
+            }
+
+            if(isGoToJoin) {
+                auth.createUserWithEmailAndPassword(email, password1).addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "성공", Toast.LENGTH_LONG).show()
+
+                            val intent =  Intent(this, MainActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                            startActivity(intent)
+
+                        } else {
+                            Toast.makeText(this, "실패", Toast.LENGTH_LONG).show()
+                        }
+                    }
+            }
+
+
+
+        }
+
+
+        // 신규 사용자 가입
+        /*
+        auth.createUserWithEmailAndPassword("abc@abc.com", "abcdabcd")
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    Toast.makeText(this, "성공", Toast.LENGTH_LONG).show()
+
+                } else {
+
+                    Toast.makeText(this, "실패", Toast.LENGTH_LONG).show()
+
+                }
+            }
+         */
+
     }
 }
