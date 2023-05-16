54221f592c9a097dfee48b62f7672eb10e8ef7a9
ouob <kyjin320@sookmyung.ac.kr>
2023-05-16 21:50:08 +0900

    Community App(Section 3)

---
+++ app/src/main/java/smu/it/mysololife/fragments/BookmarkFragment.kt
@@ -0,0 +1,49 @@
+package smu.it.mysololife.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import smu.it.mysololife.R
+import smu.it.mysololife.databinding.FragmentBookmarkBinding
+
+class BookmarkFragment : Fragment() {
+
+    private lateinit var binding: FragmentBookmarkBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_bookmark, container, false)
+
+        binding.homeTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_bookmarkFragment_to_homeFragment)
+        }
+
+        binding.tipTap.setOnClickListener {
+
+            it.findNavController().navigate(R.id.action_bookmarkFragment_to_tipFragment)
+        }
+
+        binding.talkTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_bookmarkFragment_to_talkFragment)
+        }
+
+        binding.storeTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_bookmarkFragment_to_storeFragment)
+        }
+
+        return binding.root
+    }
+}
